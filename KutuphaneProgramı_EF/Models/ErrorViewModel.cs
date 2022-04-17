44a43d3316fa567fff03f01f05ad3075721e36f0
oceylan51 <osmncyln51@gmail.com>
2022-04-17 06:31:14 +0300

    Framework bağlantıları yapıldı
    Home Page yapılacak kullanım kılavuzu yapılacak

---
+++ KutuphaneProgramı_EF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KutuphaneProgramı_EF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ KutuphaneProgramı_EF/Models/Turler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace KutuphaneProgramı_EF.Models
+{
+    public partial class Turler
+    {
+        public Turler()
+        {
+            Kitaplars = new HashSet<Kitaplar>();
+            Yazarlars = new HashSet<Yazarlar>();
+        }
+
+        public int Id { get; set; }
+        public string TurAd { get; set; }
+
+        public virtual ICollection<Kitaplar> Kitaplars { get; set; }
+        public virtual ICollection<Yazarlar> Yazarlars { get; set; }
+    }
+}
